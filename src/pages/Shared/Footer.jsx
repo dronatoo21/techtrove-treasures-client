79e5192e11838f121d0fb003cea32ff8f93912da
dronatoo <mrdronatoo@gmail.com>
2023-10-20 04:24:45 -0700

    completed footer section

--- src/pages/Shared/Footer.jsx
+++ src/pages/Shared/Footer.jsx
@@ -1,7 +1,21 @@
+import { FaFacebookF, FaTwitterSquare, FaInstagram } from 'react-icons/fa'
 const Footer = () => {
     return (
         <div>
-           <h1>Footer section</h1> 
+           <footer className="footer p-10 bg-neutral flex justify-between md:flex-row flex-col text-neutral-content">
+              <aside>
+               <img className='w-20' src="https://i.ibb.co/M7sMZmk/logo.png" alt="logo" />
+                <p>TechTrove Treasures<br/>Providing reliable tech since 1992</p>
+              </aside> 
+              <nav>
+                <header className="footer-title mr-16">Social</header> 
+                <div className="grid grid-flow-col gap-4">
+                    <FaFacebookF/>
+                    <FaInstagram/>
+                    <FaTwitterSquare/>
+                </div>
+              </nav>
+            </footer>
         </div>
     );
 };
