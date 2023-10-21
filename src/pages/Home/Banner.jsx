8e592ef9631422c346a3e697e2fe52f60605e1fc
dronatoo <mrdronatoo@gmail.com>
2023-10-21 00:36:18 -0700

    added border radius on banner

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,7 +1,7 @@
 const Banner = () => {
     return (
         <div>
-            <div className="bg-gradient-to-r from-gray-400 to bg-gray-700 items-center">
+            <div className="bg-gradient-to-r from-gray-400 to bg-gray-700 rounded-xl items-center">
                 <div className="flex items-center justify-between px-5 md:px-5 lg:px-24 md:py-10">
                   <h1 className="mb-5 text-2xl md:text-5xl text-gray-800 font-bold">Welcome to <span className="text-white">TechTrove</span></h1>
                   <img className="w-48 md:w-96 lg:w-[500px]" src="https://i.ibb.co/wy4X6cN/bannerpic.png" alt="banner-pic" />
