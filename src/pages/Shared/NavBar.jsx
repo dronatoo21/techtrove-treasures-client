9a220346017e99de7b3ab6f566d471d445a50c67
dronatoo <mrdronatoo@gmail.com>
2024-05-27 07:17:48 -0700

    completed navbar

--- src/pages/Shared/NavBar.jsx
+++ src/pages/Shared/NavBar.jsx
@@ -1,13 +1,54 @@
+import { useContext, useEffect, useState } from "react";
 import { FaList } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+import { FiSun } from "react-icons/fi";
+import { BiSolidMoon } from "react-icons/bi";
+import { toast } from "react-toastify";
 
 const NavBar = () => {
+    const [mode, setMode] = useState("light");
+
+    function changeTheme () {
+      const html = document.documentElement
+      if(mode === "light") {
+        html.classList.remove("light")
+        html.classList.add("dark")
+        setMode("dark")
+        localStorage.setItem("mode", "dark")
+      } else{
+        html.classList.remove("dark")
+        html.classList.add("light")
+        setMode("light")
+        localStorage.setItem("mode", "light")
+      }
+    }
+
+    useEffect(()=>{
+      const currentMode = localStorage.getItem("mode")|| "light";
+      setMode(currentMode)  
+      const html = document.documentElement
+      html.classList.add(currentMode)
+    },[])
+
+    const {user, userLogout} = useContext(AuthContext);
 
     const links = <>
-    <li className="btn btn-outline text-white ml-2 rounded-xl font-semibold"><NavLink to="/">Home</NavLink></li>
-    <li className="btn btn-outline text-white ml-2 rounded-xl font-semibold"><NavLink to="/addProduct">Add Product</NavLink></li>
-    <li className="btn btn-outline text-white ml-2 rounded-xl font-semibold"><NavLink to="/mycart">My Cart</NavLink></li>
+    <li><NavLink style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", borderTop: isActive ? "2px solid white" : " ", background: "transparent",})} className="btn btn-sm border-none text-white ml-2 rounded-xl font-semibold" to="/">Home</NavLink></li>
+    <li><NavLink style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", borderTop: isActive ? "2px solid white" : " ", background: "transparent",})} className="btn btn-sm border-none text-white ml-2 rounded-xl font-semibold" to="/addProduct">Add Product</NavLink></li>
+    <li><NavLink style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", borderTop: isActive ? "2px solid white" : " ", background: "transparent",})} className="btn btn-sm border-none text-white ml-2 rounded-xl font-semibold" to="/mycart">My Cart</NavLink></li>
     </>
+
+    
+const handleLogout = () => {
+    userLogout()
+    .then(res => {
+      console.log(res.user);
+    })
+    .catch(error => console.log(error));
+    toast('successfully logged out')
+  };
+  console.log(user?.photoURL);
     return (
         <div className="navbar bg-gradient-to-r from-[#3d6ae9] to-[#2684fe] text-white px-10">
           <div className="navbar-start">
@@ -27,10 +68,28 @@ const NavBar = () => {
           </div>
           <div className="navbar-center">
                 {/* <img className='md:w-20 w-10' src="https://i.ibb.co/M7sMZmk/logo.png" alt="logo" /> */}
-                <h1 className="md:text-4xl ml-1 text-2xl font-extrabold">TechTrove</h1>
+                <Link to='/'><h1 className="md:text-4xl ml-1 text-2xl font-extrabold">TechTrove</h1></Link>
           </div>
           <div className="navbar-end">
-            <a className="btn">Button</a>
+          { user ?
+            <>
+          <label tabIndex={0} className="btn btn-sm mr-7 text-white btn-circle avatar mb-4">
+            <img className="rounded-full" src={user?.photoURL} alt="photo" />
+            <p className=" text-sm text-center">{user?.displayName?.split(' ').pop()}</p>
+          </label>
+            <button onClick={handleLogout} className="btn bg-transparent btn-outline text-white rounded-xl">Logout</button></>
+              : <Link to="/login"><button className="btn bg-transparent btn-outline text-white rounded-xl">Login</button></Link>
+            }
+            <div className="ml-4 md:mx-3 mt-3">
+            <label className="swap swap-rotate">
+                {/* this hidden checkbox controls the state */}
+                <input onClick={changeTheme} type="checkbox" />
+                {/* sun icon */}
+                <h1 className="swap-on fill-current w-10 h-10"><FiSun className="text-3xl"/></h1>
+                {/* moon icon */}
+                <h1 className="swap-off fill-current w-10 h-10"><BiSolidMoon className="text-3xl"/></h1>
+              </label>
+            </div>
           </div>
         </div>
     );
