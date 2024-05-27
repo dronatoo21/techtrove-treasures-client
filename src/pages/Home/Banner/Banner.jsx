b21b763e53a3013afe7d9779cab0a8512ddfb3f1
dronatoo <mrdronatoo@gmail.com>
2024-05-27 07:50:45 -0700

    added scrollbar

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import banner from '../../../assets/Banner/TechTrove.png'
 const Banner = () => {
     return (
         <div className="h-[100vh]">
-            <img className="lg:h-[90vh] md:h-[70vh] h-[40vh] w-full" src={banner} alt="" />
+            <img className="lg:h-[90vh] md:h-[70vh] h-[30vh] w-full" src={banner} alt="" />
         </div>
     );
 };
